73fe2676e7a48dd8f0caf703e02046cb6939943f
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-15 12:56:16 +0300

    added functions to fetch to data. one function to fetch users and users repos to the respectives services

--- src/app/services/respositories.services.ts
+++ src/app/services/respositories.services.ts
@@ -1,19 +1,20 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from "@angular/common/http"
+import { HttpClient } from "@angular/common/http"
+
+import { environment } from "../../environments/environment"
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class RespositoriesService {
+
     constructor(
-        private http : HttpClient
-    ){}
-    getUser(userName: string){
-    
+        private http: HttpClient
+    ) { }
+
+    getRepos(username: string) {
+        this.http.get(environment.apiUrl + username + "/repos")
     }
-    getRepos(repoName: string){
-        
-    }
-    
+
 }
--- src/app/services/users-services.ts
+++ src/app/services/users-services.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import {environment} from "../../environments/environment"
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class UsersService {
 
+    constructor(
+        private http: HttpClient
+    ) { }
+    getUser(userName: string) {
+       return this.http.get(environment.apiUrl + userName)
+    }
 }

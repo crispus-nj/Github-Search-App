2519b483ee17d9faf3e44c1ca2f4610bb7c83dde
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-15 13:05:39 +0300

    added routing to the project, made the link active on a clicked link. added two components for users and repositories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "users", component: UsersComponent },
+  { path: "repos", component: RepositoriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

3af2f74cd0462b01b8e30f024a3d620708098d4e
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-01-16 00:17:13 +0300

    added styling to the repos section in the user component. added another section in the users component to display the users repo headings

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RespositoriesService } from '../services/respositories.services';
 import { UsersService } from '../services/users-services';
 
 @Component({
@@ -8,10 +9,12 @@ import { UsersService } from '../services/users-services';
 })
 export class UsersComponent implements OnInit {
   users: any = []
+  userRepoData: any = []
   @ViewChild('form') signup: ElementRef
 
   constructor(
-    private userService: UsersService
+    private userService: UsersService,
+    private userRepo : RespositoriesService
   ) { }
 
   ngOnInit(): void {
@@ -22,9 +25,19 @@ export class UsersComponent implements OnInit {
   getRepos(user: any) {
     this.userService.getUser(user.value).subscribe(data => {
       this.users = data
-      console.log("User Data", data)
+      // console.log("User Data", data)
     }, err => {
-      console.log("Error Occurred", err)
+      // console.log("Error Occurred", err)
     })
+
+    // get user repos
+    this.userRepo.getRepos(user.value).toPromise().then(
+      data => {
+        this.userRepoData = data
+        console.log("User repo Data", data)
+      }, err => {
+        console.log("User repo Err", err)
+      }
+    )
   }
 }
